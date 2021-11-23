1e9c03e1175905fc6d53c4c32a5b70c040c405a0
chandantiwari1 <84284576+chandantiwari1@users.noreply.github.com>
2021-11-23 14:35:37 +0530

    Delete hello.js
    unwanted file deleted]

--- hello.js
+++
@@ -1,7 +0,0 @@
-// Javascript objects 
-
-// there are two ways to create objects {} and new object(
-    var obj = new Object();
-    var testarray = [1,23,4,5];
-    obj.array = testarray;
-    console.log(obj);
