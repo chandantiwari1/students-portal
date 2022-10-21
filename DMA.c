a039f69b65654ef6acdcb570be2c6cecc16571d1
Saurav <saurabhv84btechcse2024@kccitm.edu.in>
2022-10-22 00:55:18 +0530

    DMA program added

---
+++ DMA.c
@@ -0,0 +1,22 @@
+#include<stdio.h>  
+#include<stdlib.h>  
+int main(){  
+  int n,i,*ptr,sum=0;    
+    printf("Enter number of elements: ");    
+    scanf("%d",&n);    
+    ptr=(int*)malloc(n*sizeof(int));  //memory allocated using malloc    
+    if(ptr==NULL)                         
+    {    
+        printf("Sorry! unable to allocate memory");    
+        exit(0);    
+    }    
+    printf("Enter elements of array: ");    
+    for(i=0;i<n;++i)    
+    {    
+        scanf("%d",ptr+i);    
+        sum+=*(ptr+i);    
+    }    
+    printf("Sum=%d",sum);    
+    free(ptr);     
+return 0;  
+}    
