6f8bcc6e69730b1d4c1b8717d654d5da3fc9c487
hi-vishal <114975029+hi-vishal@users.noreply.github.com>
2022-10-26 21:42:02 +0530

    maxofthree.py
    to find max of three numbers

---
+++ maxofthree.py
@@ -0,0 +1,16 @@
+def maximum(a, b, c):
+  
+    if (a >= b) and (a >= c):
+        largest = a
+  
+    elif (b >= a) and (b >= c):
+        largest = b
+    else:
+        largest = c
+          
+    return largest
+  
+a = int(input("Enter first number"))
+b = int(input("Enter second number"))
+c = int(input("Enter third number"))
+print(maximum(a, b, c))
